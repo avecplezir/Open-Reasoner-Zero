de4791e5830d14575853ea1642a9b2fa6cda76b5
avecplezir <avecplezir@gmail.com>
2025-09-06 04:44:35 -0400

    upd

--- playground/binary_noncol_orz_1p5b_ppo_3.py
+++ playground/binary_noncol_orz_1p5b_ppo_3.py
@@ -70,7 +70,7 @@ class PPOExpConfig(BasePPOExpConfig):
     vllm_sync_backend: str = "gloo"  # nccl or gloo
 
     # path related settings
-    pretrain: Optional[str] = f"{prefix}/iter104/policy" # TODO: or put your downloaded model path here!
+    pretrain: Optional[str] = f"{prefix}/Qwen2.5-1.5B" # TODO: or put your downloaded model path here!
     reward_pretrain: Optional[str] = None
     save_interval: int = 50
     # current date and time
